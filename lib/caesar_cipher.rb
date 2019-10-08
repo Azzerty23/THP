ac0e70fd4747f1f2dd7fce98e2be812080679b89
Azzerty23 <azzerty23@hotmail.fr>
2019-10-08 19:29:51 +0200

    fix caesar_cipher

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,36 +1,28 @@
-def encoder()
+def caesar_cipher(message, key=5)
+  
+  unless message.is_a?(String) && key.is_a?(Integer)
+    return "The message must be a String and the key an Integer"
+  end
+  
   encoded_message = []
   letters = message.split("")
-  letters.each do |letter|
-    if letter.is_a?(String) && key.is_a?(Integer)
+  
+  letters.each do |letter|  
       letter_ascii = letter.downcase.ord
       encoded_letter = letter_ascii + key
-      if /\s/.match?(letter)
+      if letter.match?(/\s/)
         encoded_letter = letter
       elsif encoded_letter > ("z".ord - key)
-        encoded_letter -= 25
+        encoded_letter -= 26
       end
-      encoded_message << encoded_letter
-    else
-      return "The message must be a String and the key an Integer"
+      if letter.match?(/[[:upper:]]/)
+        encoded_message << encoded_letter.chr.upcase
+      else
+        encoded_message << encoded_letter.chr
+      end
     end
-  end
-
-  return encoded_message.join(" ")
+  return encoded_message.join("")
 end
 
-def decoder()
-
-end
-
-def caesar_cipher(message, key=5)
-
-
-end
-
-print caesar_cipher("hello you")
-
-puts "a".ord
-puts "z".ord
-
-# print /\s/.match?("qsdf")
+print caesar_cipher("helLo you")
+print caesar_cipher("Z", 1)
