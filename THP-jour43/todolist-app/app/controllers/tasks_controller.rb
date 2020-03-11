6a6f813d87df3fade57ae52d9adc5f45cebc33c2
Azzerty23 <azzerty23@hotmail.fr>
2020-03-11 11:51:44 +0100

    THP-jour43

---
+++ THP-jour43/todolist-app/app/controllers/tasks_controller.rb
@@ -0,0 +1,76 @@
+class TasksController < ApplicationController
+before_action :authenticate_user!
+  def new
+    @categories = Category.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @category = Category.find(category_params)
+    @task.category = @category
+    if @task.save
+      respond_to do |format|
+        format.html {redirect_to root_path}
+        format.js { }
+      end
+      flash[:notice] = "Task created"
+    else
+      respond_to do |format|
+        format.html {
+          flash[:notice] = "Please try again"
+          redirect_to root_path
+        }
+      end
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    @categories = Category.all
+
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    respond_to do |format|
+      format.html {
+        flash[:notice] = "Task updated (HTML)"
+        redirect_to root_path
+      }
+      format.js {
+        flash.now[:notice] = "Task updated (AJAX)"
+      }
+    end
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    respond_to do |format|
+      format.html {
+        flash[:notice] = "Ok, it's gone for good. (HTML)"
+        redirect_to root_path
+      }
+      format.js {
+        flash.now[:notice] = "Task destroyed (AJAX)"
+      }
+    end
+  end
+
+
+  private
+
+  def task_params
+    params.permit(:title, :deadline, :description, :status)
+  end
+
+  def category_params
+    params.require(:Category)
+  end
+
+end
